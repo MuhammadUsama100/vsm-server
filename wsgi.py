41b1f945a3555581ab7c4905a3e19a54a1b93b19
Muhammad Usama <46392511+MuhammadUsama100@users.noreply.github.com>
2021-05-02 19:42:34 +0500

    deploy

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from query_processing import app
+from OpenSSL import SSL
+# context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+# context.use_privatekey_file('server.key')
+# context.use_certificate_file('server.crt')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", ssl_context='adhoc')
